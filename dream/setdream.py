e61c661fa7190691c1917c29e444b06d6cf92941
Tamás Hauer <Tamas.Hauer@prodema.ch>
2015-07-16 10:09:57 +0200

    adds ssh_config to the repo

--- dream/setdream.py
+++ dream/setdream.py
@@ -4,18 +4,20 @@ import sys
 import xmlrpclib
 import gandi
 
-def setIPForDream(ipaddress):
+def setIPForName(ipaddress, name="dream"):
   """ Sets the A-record for the name "dream" in the zone specified by gandi.zoneid
   """
 
   api=xmlrpclib.ServerProxy('https://rpc.gandi.net/xmlrpc/')
   version = api.domain.zone.version.new(gandi.apikey, gandi.zoneid)
-  api.domain.zone.record.delete(gandi.apikey, gandi.zoneid, version, {"type" : "A", "name": "dream"})
+  api.domain.zone.record.delete(gandi.apikey, gandi.zoneid, version, {"type" : "A", "name": name})
   api.domain.zone.record.add(gandi.apikey, gandi.zoneid, version, 
-    {"type" : "A", "name": "dream", "value": ipaddress, "ttl": 300 })
+    {"type" : "A", "name": name, "value": ipaddress, "ttl": 300 })
   api.domain.zone.version.set(gandi.apikey, gandi.zoneid, version)
 
 if len(sys.argv) == 2:
-  setIPForDream(sys.argv[1])
+  setIPForName(sys.argv[1])
+elif len(sys.argv) == 3:
+  setIPForName(sys.argv[1], name=sys.argv[2])
 else:
-  print "Usage: setdream.py <ipaddress>"
+  print "Usage: setdream.py <ipaddress> [name]"
