53b92bef857ae87edc582244f0a6859feddde004
Tamás Hauer <tamas.hauer@gmail.com>
2015-10-29 09:33:15 +0100

    Start remote example

---
+++ webrtcbook/js/remotePeerConnection.js
@@ -0,0 +1,95 @@
+var localStream, localPeerConnection, remotePeerConnection;
+
+var localVideo = document.getElementById("localVideo");
+var remoteVideo = document.getElementById("remoteVideo");
+
+var startButton = document.getElementById("startButton");
+var callButton = document.getElementById("callButton");
+var hangupButton = document.getElementById("hangupButton");
+
+startButton.disabled = false;
+callButton.disabled = true;
+hangupButton.disabled = true;
+
+startButton.onclick = function() {
+  startButton.disabled = true;
+  console.log("%o", navigator);
+  navigator.webkitGetUserMedia({audio:true, video:true}, 
+    function(stream) {
+      console.log("getUserMedia.callback( %o )", stream)
+      localVideo.src = URL.createObjectURL(stream);
+      localStream = stream;
+      callButton.disabled = false;
+    }, 
+    function(error){ 
+      console.log("navigator.getUserMedia error: ", error); 
+    }
+  );
+};
+
+var pc_config = {'iceServers': [{'url': 'stun:stun.l.google.com:19302'}]};
+var pc_constraints = {'optional': [{'DtlsSrtpKeyAgreement': true}]};
+
+callButton.onclick = function() {
+  callButton.disabled = true;
+  hangupButton.disabled = false;
+
+  var servers = null;
+
+  localPeerConnection = new webkitRTCPeerConnection(servers);
+  console.log("localPC = %o.new()", localPeerConnection);
+  remotePeerConnection = new webkitRTCPeerConnection(servers);
+  console.log("rempotePC = %o.new()", remotePeerConnection);
+
+  localPeerConnection.onicecandidate = function(event) {
+    if(event.candidate) {
+      remotePeerConnection.addIceCandidate(event.candidate);
+      console.log("Local ICE candidate: %o %s", event, event.candidate.candidate);
+    }
+  };
+
+  remotePeerConnection.onicecandidate = function(event) {
+    if(event.candidate) {
+      localPeerConnection.addIceCandidate(event.candidate);
+      console.log("Remote ICE candidate: %o %s", event, event.candidate.candidate);
+    }
+  };
+
+  remotePeerConnection.onaddstream = function(event){
+    console.log("remotePeerConnection.onaddstream( %o )", event);
+    remoteVideo.src = window.URL.createObjectURL(event.stream);
+  };
+
+  console.log("%o.addStream( %o )", localPeerConnection, localStream);
+  localPeerConnection.addStream(localStream);
+
+  localPeerConnection.createOffer(
+    function(offer) {
+      console.log("createOffer.callback( %o )", offer);
+      localPeerConnection.setLocalDescription(offer);
+      remotePeerConnection.setRemoteDescription(offer);
+      remotePeerConnection.createAnswer(
+        function(answer) {
+          console.log("createAnswer.callback( %o )", answer);
+          remotePeerConnection.setLocalDescription(answer);
+          localPeerConnection.setRemoteDescription(answer);
+        },
+        function(error){ console.log("Failed to create signaling message: " + error.name )}
+      );
+      console.log("returned from createAnswer()");
+    }
+  );
+  console.log("returned from createOffer()");
+};
+
+hangupButton.onclick = function() {
+  localPeerConnection.close();
+  remotePeerConnection.close();
+  console.log("hangup: %o, %o", localPeerConnection, remotePeerConnection);
+
+  localPeerConnection = null;
+  remotePeerConnection = null;
+
+  hangupButton.disabled = true;
+  callButton.disabled = false;
+}
